ef2a07cc5e073b1d74447b457e607d4121998c42
SNDP-Design <sndpdesign@gmail.com>
2026-07-12 13:51:54 +0530

    Change default editor aspect ratio setting to 16:9

--- src/constants/presets.ts
+++ src/constants/presets.ts
@@ -13,7 +13,7 @@ export const DEFAULT_SETTINGS: EditorSettings = {
   backgroundType: 'gradient',
   gradientPresetId: 'nebula',
   solidColor: '#0f172a',
-  aspectRatio: '16-10',
+  aspectRatio: '16-9',
   borderRadius: 24,
   shadowIntensity: 60,
   borderWidth: 1,
